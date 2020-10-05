ea184b7bdba42fda364f3e81504923a23f779ed7
AmeliaWen <cuiwen423@gmail.com>
2020-10-05 15:11:40 -0400

    removed default

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/service/TestEventRegistrationService.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/service/TestEventRegistrationService.java
@@ -1,10 +1,57 @@
 package ca.mcgill.ecse321.eventregistration.service;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import ca.mcgill.ecse321.eventregistration.dao.EventRepository;
+import ca.mcgill.ecse321.eventregistration.dao.PersonRepository;
+import ca.mcgill.ecse321.eventregistration.dao.RegistrationRepository;
+import ca.mcgill.ecse321.eventregistration.model.Event;
+import ca.mcgill.ecse321.eventregistration.model.Person;
+import ca.mcgill.ecse321.eventregistration.model.Registration;
 
 
 @ExtendWith(MockitoExtension.class)
 public class TestEventRegistrationService {
+	@Mock
+	private PersonRepository personDao;
+
+	@InjectMocks
+	private EventRegistrationService service;
+
+	private static final String PERSON_KEY = "TestPerson";
+
+	@BeforeEach
+	public void setMockOutput() {
+	    lenient().when(personDao.findPersonByName(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
+	        if(invocation.getArgument(0).equals(PERSON_KEY)) {
+	            Person person = new Person();
+	            person.setName(PERSON_KEY);
+	            return person;
+	        } else {
+	            return null;
+	        }
+	    });
+	}
 
 }
